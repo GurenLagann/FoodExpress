e5bad83732d3440fc08ebad60d97244017577faf
GurenLagann <dragonwits@gmail.com>
2019-11-02 21:21:42 -0300

    criando a database

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,8 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,6 +14,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase":"off",
